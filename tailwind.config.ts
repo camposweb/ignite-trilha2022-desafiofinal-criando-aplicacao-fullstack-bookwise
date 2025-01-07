5bfa5f09f4331190337b442a01b76091770b4660
Leandro Campos <camposweb@gmail.com>
2025-01-07 18:12:14 -0300

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          100: '#50B2C0',
+          200: '#255D6A',
+          300: '#0A313C',
+        },
+
+        purple: {
+          100: '#8381D9',
+          200: '#2A2879',
+        },
+
+        gray: {
+          100: '#F8F9FC',
+          200: '#E6E8F2',
+          300: '#D1D6E4',
+          400: '#8D95AF',
+          500: '#303F73',
+          600: '#252D4A',
+          700: '#181C2A',
+          800: '#0E1116',
+        },
+      },
+      lineHeight: {
+        short: '140%',
+        base: '160%',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
