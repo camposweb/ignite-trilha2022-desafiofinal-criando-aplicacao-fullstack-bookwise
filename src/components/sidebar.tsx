9109aa08a8db7bbb1038f47b0d892d0ba76fe26a
Leandro Campos <camposweb@gmail.com>
2025-01-08 22:42:34 -0300

    feat: Ajutes no componente sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import { tv, VariantProps } from 'tailwind-variants'
 
 const sidebar = tv({
   base: [
-    'w-60 rounded-xl justify-between py-10 pb-6 m-5 lg:flex flex-col bg-no-repeat bg-cover bg-center hidden bg-sidebar h-[calc(100%-40px)] items-center',
+    'min-w-60 rounded-xl justify-between py-10 pb-6 m-5 lg:flex flex-col bg-no-repeat bg-cover bg-center hidden bg-sidebar h-[calc(100%-40px)] items-center',
   ],
 })
 
