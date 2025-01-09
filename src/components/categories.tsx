e74b48be62870055d4bc84c20a492b9fbe1781da
Leandro Campos <camposweb@gmail.com>
2025-01-09 00:03:15 -0300

    feat: Implementando o componente categories

---
+++ src/components/categories.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const categories = tv({
+  base: [
+    'bg-transparent border border-solid border-purple-100 rounded-full py-2 px-5 transition',
+    'text-purple-100 font-nunito font-normal leading-base text-base',
+    'hover:text-gray-100 hover:bg-purple-200 hover:border-purple-100',
+  ],
+})
+
+type CategoryType = ComponentProps<'button'> & VariantProps<typeof categories>
+
+export const Categories = ({ className, ...props }: CategoryType) => {
+  return <button {...props} className={categories({ className })} />
+}
