3fc0bf72035bc5af3e995f614180292e51ad19ba
Leandro Campos <camposweb@gmail.com>
2025-01-10 02:09:06 -0300

    feat: Implementando componente recent-reviews

---
+++ src/components/recent-reviews.tsx
@@ -0,0 +1,133 @@
+'use client'
+import { tv, VariantProps } from 'tailwind-variants'
+import { Box } from './box'
+import { ComponentProps, useState } from 'react'
+import { Rating, ThinRoundedStar } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+import Image from 'next/image'
+
+const customStyles = {
+  itemShapes: ThinRoundedStar,
+  activeFillColor: '#8381D9',
+  inactiveFillColor: '#000000',
+  activeStrokeColor: '#8381D9', // Borda para estrelas inativas
+  inactiveStrokeColor: '#8381D9', // Borda para estrelas inativas
+  itemStrokeWidth: 2, // Largura da borda
+}
+
+const recentReviews = tv({
+  extend: Box,
+  slots: {
+    base: 'flex flex-col w-full gap-5',
+    header: 'flex justify-between',
+    wrapper: 'flex flex-col',
+    titleBook: 'font-nunito text-base font-bold leading-short text-gray-100',
+    authorBook: 'text-sm font-normal leading-base text-gray-400',
+    footer: 'mt-auto flex w-full',
+    review: 'mt-auto flex w-full',
+    sinopse: 'font-nunito text-sm font-normal text-gray-300 line-clamp-4',
+  },
+})
+
+const {
+  base,
+  header,
+  wrapper,
+  titleBook,
+  authorBook,
+  footer,
+  // review,
+  sinopse,
+} = recentReviews()
+
+type RecentReviewsType = ComponentProps<typeof Box> &
+  VariantProps<typeof recentReviews>
+
+interface RecentReviewsProps extends RecentReviewsType {
+  userAvatar: string
+  userName: string
+  dateReview: string
+  coverUrl: string
+  title: string
+  author: string
+  rating: number
+  userReview: string
+}
+
+export const RecentReviews = ({
+  userAvatar,
+  userName,
+  dateReview,
+  coverUrl,
+  title,
+  author,
+  rating,
+  userReview,
+  ...props
+}: RecentReviewsProps) => {
+  const [textExpand, setTextExpand] = useState(false)
+
+  const limit = 200
+
+  function handleExpandText() {
+    setTextExpand(!textExpand)
+  }
+  return (
+    <Box {...props}>
+      <figure className={base()}>
+        <header className={header()}>
+          <div className="flex gap-2">
+            <Image
+              src={userAvatar}
+              alt={userName}
+              width={40}
+              height={40}
+              quality={100}
+              className="h-10 w-10 rounded-full border border-solid border-green-100 bg-cover"
+            />
+            <div className="flex flex-col">
+              <span className="font-nunito text-base font-normal leading-base text-gray-100">
+                {userName}
+              </span>
+              <span className="text-sm font-normal leading-base text-gray-400">
+                {dateReview}
+              </span>
+            </div>
+          </div>
+          <div>
+            <Rating
+              value={rating}
+              readOnly
+              itemStyles={customStyles}
+              style={{ maxWidth: 92 }}
+            />
+          </div>
+        </header>
+        <div className="flex gap-5">
+          <Image
+            src={coverUrl}
+            alt={coverUrl}
+            width={108}
+            height={152}
+            className="h-[152px] w-[108px]"
+          />
+          <div className={wrapper()}>
+            <h1 className={titleBook()}>{title}</h1>
+            <h3 className={authorBook()}>{author}</h3>
+            <footer className={footer()}>
+              <p className={sinopse()}>
+                {textExpand ? userReview : `${userReview.slice(0, limit)}...`}
+                <button
+                  onClick={handleExpandText}
+                  className="ml-2 font-nunito text-sm font-bold leading-base text-purple-100"
+                >
+                  {textExpand ? 'Ver menos' : 'Ver mais'}
+                </button>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </figure>
+    </Box>
+  )
+}
