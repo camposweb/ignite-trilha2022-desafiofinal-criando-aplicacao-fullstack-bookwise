2fd24b437180d813b6d72a90476dc80cea59fb58
Leandro Campos <camposweb@gmail.com>
2025-01-10 02:29:05 -0300

    feat: Ajustes no componente card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,6 @@
-'use client'
 import Image from 'next/image'
-import { ComponentProps, useState } from 'react'
+import { ComponentProps } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
-import avatar from '../app/assets/avatar.png'
 import { Rating, ThinRoundedStar } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
 
@@ -28,24 +26,14 @@ const card = tv({
   },
   variants: {
     variants: {
-      recents: 'flex-col',
-      populars: '',
+      read: '',
     },
   },
 })
 
 type CardType = ComponentProps<'figure'> & VariantProps<typeof card>
 
-const {
-  base,
-  header,
-  titleBook,
-  authorBook,
-  footer,
-  wrapper,
-  sinopse,
-  footerSinopse,
-} = card()
+const { base, titleBook, authorBook, footer, wrapper } = card()
 
 interface CardProps extends CardType {
   title: string
@@ -62,100 +50,32 @@ export const Card = ({
   author,
   coverUrl,
   rating,
-  member,
-  avalaliationDate,
   variants,
   ...props
 }: CardProps) => {
-  const [textExpand, setTextExpand] = useState(false)
-
-  const text = `Semper et sapien proin vitae nisi. Feugiat neque integer donec
-                  et aenean posuere amet ultrices. Cras fermentum id pulvinar
-                  varius leo a in. Amet libero pharetra nunc elementum fringilla
-                  velit ipsum. Sed vulputate massa velit nibh`
-
-  const limit = 200
-
-  function handleExpandText() {
-    setTextExpand(!textExpand)
-  }
   return (
     <figure {...props} className={base({ variants })}>
-      {member ? (
-        <>
-          <header className={header()}>
-            <div className="flex gap-2">
-              <div className="h-10 w-10">
-                <Image src={avatar} alt={member} width={40} height={40} />
-              </div>
-              <div className="flex flex-col">
-                <span className="font-nunito text-base font-normal leading-base text-gray-100">
-                  {member}
-                </span>
-                <span className="text-sm font-normal leading-base text-gray-400">
-                  {avalaliationDate}
-                </span>
-              </div>
-            </div>
-            <div>
-              <Rating
-                value={rating}
-                readOnly
-                itemStyles={customStyles}
-                style={{ maxWidth: 92 }}
-              />
-            </div>
-          </header>
-          <div className="flex gap-5">
-            <Image
-              src={coverUrl}
-              alt={coverUrl}
-              width={108}
-              height={152}
-              className="h-[152px] w-[108px]"
-            />
-            <div className={wrapper()}>
-              <h1 className={titleBook()}>{title}</h1>
-              <h3 className={authorBook()}>{author}</h3>
-              <footer className={footerSinopse()}>
-                <p className={sinopse()}>
-                  {textExpand ? text : `${text.slice(0, limit)}...`}
-                  <button
-                    onClick={handleExpandText}
-                    className="ml-2 font-nunito text-sm font-bold leading-base text-purple-100"
-                  >
-                    {textExpand ? 'Ver menos' : 'Ver mais'}
-                  </button>
-                </p>
-              </footer>
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <Image
-            src={coverUrl}
-            alt={coverUrl}
-            width={64}
-            height={94}
-            className="h-[94px] w-[64px]"
+      <Image
+        src={coverUrl}
+        alt={coverUrl}
+        width={64}
+        height={94}
+        className="h-[94px] w-[64px]"
+      />
+      <div className={wrapper()}>
+        <figcaption>
+          <h1 className={titleBook()}>{title}</h1>
+          <h3 className={authorBook()}>{author}</h3>
+        </figcaption>
+        <footer className={footer()}>
+          <Rating
+            value={rating}
+            readOnly
+            itemStyles={customStyles}
+            style={{ maxWidth: 92 }}
           />
-          <div className={wrapper()}>
-            <figcaption>
-              <h1 className={titleBook()}>{title}</h1>
-              <h3 className={authorBook()}>{author}</h3>
-            </figcaption>
-            <footer className={footer()}>
-              <Rating
-                value={4}
-                readOnly
-                itemStyles={customStyles}
-                style={{ maxWidth: 92 }}
-              />
-            </footer>
-          </div>
-        </>
-      )}
+        </footer>
+      </div>
     </figure>
   )
 }
