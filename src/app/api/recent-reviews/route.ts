ddca65f4c5dd9041c93463eb92b98b112bc6e384
Leandro Campos <camposweb@gmail.com>
2025-01-09 23:12:01 -0300

    feat: Implementando rota com os dados das avaliações mais recentes

---
+++ src/app/api/recent-reviews/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  if (req.method !== 'GET') {
+    return NextResponse.json({ error: 'Method not allowed' }, { status: 405 })
+  }
+  try {
+    const recentsReviews = await prisma.rating.findMany({
+      orderBy: { created_at: 'desc' },
+      include: {
+        book: true,
+        User: true,
+      },
+    })
+
+    return NextResponse.json({ recentsReviews }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Algum erro ocorreu' + error },
+      { status: 500 },
+    )
+  }
+}
