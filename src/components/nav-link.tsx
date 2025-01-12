c2391df13e8735c7e74360f136022e6959f04293
Leandro Campos <camposweb@gmail.com>
2025-01-11 21:56:31 -0300

    fix: Realizando alguns ajustes no componente NavLink

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -6,7 +6,11 @@ import { tv, VariantProps } from 'tailwind-variants'
 
 const link = tv({
   base: [
-    'flex gap-2 font-nunito text-base font-bold leading-base text-gray-400 transition hover:text-gray-100 data-[active=true]:text-gray-100',
+    'flex gap-4 w-full font-nunito text-base font-bold leading-base text-gray-400 transition hover:text-gray-100 data-[active=true]:text-gray-100',
+    'before:content-[""] before:w-1 before:h-6 before:opacity-0 before:rounded-full before:transition',
+    'before:bg-gradient-to-t from-[#9694F5] from-0% to-[#7FD1CC] to-100%',
+    'data-[active=true]:before:opacity-100 hover:before:opacity-100',
+    '',
   ],
 })
 
