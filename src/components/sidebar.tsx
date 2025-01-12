26d2baa5db4f88517d966d5246c528ad09cf13e7
Leandro Campos <camposweb@gmail.com>
2025-01-11 21:58:37 -0300

    fix: ajustes no componente SideBar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,17 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
 import { ComponentProps } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
+import { NavLink } from './nav-link'
+import {
+  Binoculars,
+  ChartLineUp,
+  SignIn,
+  User,
+} from '@phosphor-icons/react/dist/ssr'
+import logoBookWise from '../app/assets/logo-bookwise.svg'
+import { useSession } from 'next-auth/react'
 
 const sidebar = tv({
   base: [
@@ -10,5 +22,37 @@ const sidebar = tv({
 type SidebarType = ComponentProps<'aside'> & VariantProps<typeof sidebar>
 
 export const SideBar = ({ className, ...props }: SidebarType) => {
-  return <aside {...props} className={sidebar({ className })} />
+  const { data: session } = useSession()
+  return (
+    <aside {...props} className={sidebar({ className })}>
+      <Link href={''} className="flex items-center gap-2">
+        <Image src={logoBookWise} alt="Logo" className="h-8 w-32" />
+      </Link>
+      <nav className="mb-auto mt-16 flex flex-col gap-4">
+        <NavLink href={'/'}>
+          <ChartLineUp size={24} />
+          Início
+        </NavLink>
+        <NavLink href={'/explorar'}>
+          <Binoculars size={24} />
+          Explorar
+        </NavLink>
+        {session && (
+          <NavLink href={'/perfil'}>
+            <User size={24} />
+            Perfil
+          </NavLink>
+        )}
+      </nav>
+      <footer className="flex justify-end">
+        <Link
+          href={'/login'}
+          className="flex gap-2 font-nunito text-base font-bold leading-base text-gray-100"
+        >
+          Fazer Login
+          <SignIn size={24} className="text-green-100" />
+        </Link>
+      </footer>
+    </aside>
+  )
 }
