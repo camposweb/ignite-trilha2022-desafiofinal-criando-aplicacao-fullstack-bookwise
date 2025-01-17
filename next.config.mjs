4d76747c3fdc5f905aa4b735f1fe109bf1681af8
Leandro Campos <camposweb@gmail.com>
2025-01-16 21:54:05 -0300

    fix: Correções deploy Vercel 02

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [
       {
