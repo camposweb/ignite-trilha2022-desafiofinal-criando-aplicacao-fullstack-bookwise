b87db580a83b1992696d4202eb4244095fdce029
Leandro Campos <camposweb@gmail.com>
2025-01-17 01:15:58 -0300

    fix: Correções deploy Vercel 09

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    AUTH_SECRET: z.string(),
     AUTH_GOOGLE_ID: z.string(),
     AUTH_GOOGLE_SECRET: z.string(),
     AUTH_GITHUB_ID: z.string(),
